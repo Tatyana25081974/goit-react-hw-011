bd7180db966204b2ee0d4e6ec030b04bc935f707
Tatyana <tatyanakorabelnikovamama@gmail.com>
2025-02-21 19:04:12 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import Profile from './components/Profile/Profile';
-import userData from '../userData.json';
+import userData from './userData.json';
 
 function App() {
   const [count, setCount] = useState(0)
